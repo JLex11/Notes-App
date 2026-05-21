a6982dfbfba97f81d38d989c38c52b4d722eb2cd
Lexx <93395794+JLex11@users.noreply.github.com>
2026-05-20 19:32:54 -0500

    Auto-login and skip Welcome screen - go directly to App

--- src/index.jsx
+++ src/index.jsx
@@ -1,25 +1,36 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Provider, useSelector } from 'react-redux'
+import { Provider } from 'react-redux'
 import App from './App'
-import Welcome from './Welcome'
 import { store } from './redux/store/store'
+import loginRequest from './services/loginRequest'
+import notesRequest from './services/notesRequest'
 import * as serviceWorkerRegistration from './serviceWorkerRegistration'
 import './styles/index.css'
 
-const IndexSwitcher = () => {
-  const user = useSelector(state => state.user) || JSON.parse(localStorage.getItem('loggedNoteAppUser'))
-  return user ? <App /> : <Welcome />
+// Auto-login on app start
+const autoLogin = async () => {
+  const existing = localStorage.getItem('loggedNoteAppUser')
+  if (!existing) {
+    const user = await loginRequest.login({ username: 'User' })
+    localStorage.setItem('loggedNoteAppUser', JSON.stringify(user))
+    notesRequest.setToken(user.token)
+  } else {
+    const user = JSON.parse(existing)
+    notesRequest.setToken(user.token)
+  }
 }
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <IndexSwitcher />
-    </Provider>
-  </React.StrictMode>
-)
+autoLogin().then(() => {
+  const root = ReactDOM.createRoot(document.getElementById('root'))
+  root.render(
+    <React.StrictMode>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </React.StrictMode>
+  )
+})
 
 serviceWorkerRegistration.register({
   onUpdate: async registration => {
